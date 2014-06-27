56d61b7ebb3b73aa9edfb6feed056542f1dbec66
Amanpreet Singh <apsdehal@gmail.com>
2014-06-27 23:12:55 +0530

    Makes full-content block workable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 //Block Hovering
-$('.block-work').hover(
-	function(){//Hover in
-	$(this).find('.hover').slideDown('slow');
-	},
-	function(){//Hover out
-	$(this).find('.hover').slideUp('slow');
+$(document).ready(function(){
+	$('.block-work').hover(
+		function(){//Hover in
+		$(this).find('.hover').slideDown('slow');
+		},
+		function(){//Hover out
+		$(this).find('.hover').slideUp('slow');
+		});
+	$('.block-work .excerpt a').click( function(){
+		$(this).parent().parent().next().toggle('fast');
 	});
-$('.block-work .excerpt a').click( function(e){
-	e.preventDefault();
-	console.log('hi');
-	$(this).parent().parent().next().toggle('fast');
 });

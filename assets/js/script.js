20468413c5070326e9a6fec96f9e73b8acd700ba
Amanpreet Singh <apsdehal@gmail.com>
2014-06-27 20:26:22 +0530

    Adds css and js for hover based excerpt display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,10 @@
-// History.Adapter.bind(window,'statechange',function(){ // Note: We are using statechange instead of popstate
-// 	var state = History.getState();
-// 	$.get(state.url, function(html){
-// 		$('main')[0].innerHTML=$(html).find('div>main')[0].innerHTML;
-// 		$('title')[0].innerText=$(html).filter('title')[0].innerText;
-// 	})
-// });
-// //Someone clicked on a link in the left pane
-// $('nav a').click(function(e){
-// 	var link = e.target.getAttribute('href');//Should be relative
-// 	History.pushState(null, null, link);
-// 	e.preventDefault();
-// });
-
 //Block Hovering
 $(function(){
-	$('.block-work').hover(function(){
-		$(this).find('.hover').slideToggle('fast');
-	})
+	$('.block-work').hover(
+		function(){//Hover in
+		$(this).find('.hover').slideDown('slow');
+		},
+		function(){//Hover out
+		$(this).find('.hover').slideUp('slow');
+		});
 });

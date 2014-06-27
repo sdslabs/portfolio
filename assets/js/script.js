50ae3155b97438b01a64c29e304f39e6a14c31b0
Amanpreet Singh <apsdehal@gmail.com>
2014-06-27 21:26:41 +0530

    Makes small hacks with js for read more

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
 //Block Hovering
-$(function(){
-	$('.block-work').hover(
-		function(){//Hover in
-		$(this).find('.hover').slideDown('slow');
-		},
-		function(){//Hover out
-		$(this).find('.hover').slideUp('slow');
-		});
+$('.block-work').hover(
+	function(){//Hover in
+	$(this).find('.hover').slideDown('slow');
+	},
+	function(){//Hover out
+	$(this).find('.hover').slideUp('slow');
+	});
+$('.block-work .excerpt a').click( function(e){
+	e.preventDefault();
+	console.log('hi');
+	$(this).parent().parent().next().toggle('fast');
 });

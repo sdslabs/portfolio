b6641ea81720dc798fd22319765f4b11ab113f59
Amanpreet Singh <apsdehal@gmail.com>
2014-06-27 23:29:15 +0530

    Clicking outside the fullcontent should now close it

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,15 @@ $(document).ready(function(){
 		function(){//Hover out
 		$(this).find('.hover').slideUp('slow');
 		});
-	$('.block-work .excerpt a').click( function(){
+	$('.block-work .excerpt a').click( function(e){
 		$(this).parent().parent().next().toggle('fast');
+		e.stopPropagation();
+	});
+	$('html').click(function(e){
+		if(e.target.className != 'full-content')
+			$('.full-content').hide();
+	})
+	$('.block-work .full-content button').click( function(){
+		$(this).parent().hide();
 	});
 });

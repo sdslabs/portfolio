b6e0662d08eaccf3a85e145f7db273b743af3f1a
AyanChoudhary <darkrider251099@gmail.com>
2019-11-30 18:53:19 +0530

    fix size variations and linting issues

--- frontend/src/components/Gallery.vue
+++ frontend/src/components/Gallery.vue
@@ -1,9 +1,21 @@
 <template>
     <div class="gallery w-gallery">
-        <div><img class="h-72 w-96" src="@/assets/images/image 3.3.png" alt="image"></div>
-        <div class="textbox absolute w-88 h-56 ml-64 mt-gallery bg-white z-30">
-            <div class="font-extrabold mt-6 ml-6">Lectures</div>
-            <div class="w-72 leading-170 text-grey mt-5 ml-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac lacinia nisi, ac maximus sem. Ut eu malesuada nisl. Pellentesque sagittis fringilla tempor. Lorem ipsum dolor</div>
+        <img
+            class="h-88 md:h-128 md:w-108"
+            src="@/assets/images/image 3.3.png"
+            alt="image"
+        />
+        <div
+            class="textbox absolute w-96 h-64 ml-44 -mt-32 md:w-98 md:h-88 md:ml-98 md:mt-gallery bg-white z-30"
+        >
+            <div class="font-extrabold md:text-lg mt-10 ml-10">Lectures</div>
+            <div
+                class="w-88 md:w-96 leading-170 text-grey md:text-base mt-8 ml-10"
+            >
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+                ac lacinia nisi, ac maximus sem. Ut eu malesuada nisl.
+                Pellentesque sagittis fringilla tempor. Lorem ipsum dolor
+            </div>
         </div>
         <div>
             <span></span>

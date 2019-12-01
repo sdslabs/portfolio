331575414e167eb5e501c89bc476f9de39b31f7e
AyanChoudhary <darkrider251099@gmail.com>
2019-12-01 21:50:23 +0530

    feat: made sister group components

---
+++ frontend/src/components/SisterGroups.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="sister-groups flex flex-col content-center w-full">
+    <div class="font-black self-center text-3xl leading-loose w-feed text-center">Sister Groups</div>
+    <div class="font-semibold self-center text-base text-center leading-normal w-sister mt-16">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac lacinia nisi, ac maximus sem. Ut eu malesuada nisl. Pellentesque sagittis fringilla tempor. Pellentesque sit amet augue sit amet quam feugiat porta vitae sit amet ligula.</div>
+    <div class="flex flex-row self-center mt-32 justify-between w-groups">
+      <div>
+          <Sister url="" title="DATA SCIENCE GROUP" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac lacinia nisi, ac maximus sem."/>
+      </div>
+      <div>
+          <Sister url="" title="PAG" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac lacinia nisi, ac maximus sem."/>
+      </div>
+      <div>
+          <Sister url="" title="INFOSEC IITR" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac lacinia nisi, ac maximus sem."/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sister from "@/components/about/Sister.vue"
+export default {
+    name: "sisterGroups",
+    components: {
+        Sister
+    }
+}
+</script>
---
+++ frontend/src/components/about/Sister.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex flex-col w-144 h-96 shadow-sister">
+        <div class="flex flex-row mt-8">
+            <div><img class="ml-12 w-56 h-40" :src="url"/></div>
+            <div class="mt-12 ml-12 leading-160 font-semibold text-lg w-64">{{title}}</div>
+        </div>
+        <div class="mt-6 ml-10 mr-12 text-base leading-normal">{{description}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "sister",
+    props: {
+        url: String,
+        title: String,
+        description: String
+    }
+}
+</script>

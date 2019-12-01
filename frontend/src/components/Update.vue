3737a5a8d6346abfe337ecf58929518d461e7e21
AyanChoudhary <darkrider251099@gmail.com>
2019-12-01 22:54:28 +0530

    feat: componentisation of updatecard and fix: tailwind.js inconsistencies and minor spacing issues

--- frontend/src/components/Update.vue
+++ frontend/src/components/Update.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="flex flex-row mt-16 mb-32">
-        <div class="w-feed flex flex-col ml-68 mt-16">
-            <div class="font-extrabold text-xl">{{title}}</div>
-            <div :class="timing ? 'font-semibold text-base mt-14' : 'text-base mt-20'">{{timing ? timing : description}}</div>
-            <div class="text-base mt-14">{{description1}}</div>
-            <div class="text-base mt-14">{{description2}}</div>
-            <div class="text-base mt-14">{{description3}}</div>
+        <div class="w-feed flex flex-col ml-60 mt-16">
+            <div class="font-extrabold text-xl leading-180">{{title}}</div>
+            <div :class="timing ? 'font-semibold text-base mt-14' : 'text-base mt-20'" class="leading-normal">{{timing ? timing : description}}</div>
+            <div class="text-base mt-14 leading-normal">{{description1}}</div>
+            <div class="text-base mt-14 leading-normal">{{description2}}</div>
+            <div class="text-base mt-14 leading-normal">{{description3}}</div>
         </div>
         <div class="ml-44 shadow-image"><img class="w-image" :src="url"/></div>
     </div>
---
+++ frontend/src/components/news/UpdateCard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div class="mt-12 font-extrabold text-base leading-180">{{title}}</div>
+        <div class="mt-5 text-base leading-normal">{{description}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "updateCard",
+    props: {
+        title: String,
+        description: String
+    }
+}
+</script>

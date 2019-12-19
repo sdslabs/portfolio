03f6ae767b820eaddd991aff9adb63b26da893d8
shubhamgupta2956 <shubham.gupta2956@gmail.com>
2019-12-20 05:08:32 +0530

    add register model code

--- apps/news/admin.py
+++ apps/news/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Event, EventUpdate
+
 # Register your models here.
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('type', 'title', 'timing', 'description', 'description1', 'url', 'image', 'status')
+
+@admin.register(EventUpdate)
+class EventUpdateAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'description1', 'description2', 'url', 'image', 'event')

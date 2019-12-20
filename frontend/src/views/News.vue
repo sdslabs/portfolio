db24e8825a159dc625e7f3c9af336f20e3ba747a
shubhamgupta2956 <shubham.gupta2956@gmail.com>
2019-12-21 04:29:26 +0530

    backend integration of news page

--- frontend/src/views/News.vue
+++ frontend/src/views/News.vue
@@ -4,68 +4,74 @@
             <div class="font-black text-3xl leading-180">News Updates</div>
             <div class="w-largetext text-base mt-10 leading-normal">Lorem ipsum dolor sit amet, adipiscing elit. Aenean commodo ligula eget dolor. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Lorem ipsum dolor sit amet, adipiscing elit.</div>
         </div>
-        <div class="flex flex-row pt-28 pr-navbar">
+        <div class="flex flex-row pt-28 pr-navbar" v-for="event in event" v-bind:key="event" >
             <div class="flex flex-col">
-                <div class="w-feed h-auto shadow-feed pl-10 pt-10 pr-navbar">
-                    <Label text="UPCOMING EVENT"/>
-                    <div class="flex flex-col h-72 mt-12 pb-12">
-                        <div class="font-extrabold text-xl leading-180">SYNTAX ERROR - CAMPUS HACKATHON</div>
-                        <div class="mt-8 text-base font-semibold leading-170">27 - 30th September, 2018</div>
-                        <div class="mt-16 text-base leading-normal">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</div>
+                <div v-if="event.types =='upcoming'">
+                    <div class="w-feed h-auto shadow-feed pl-10 pt-10 pr-navbar">
+                        <Label text="UPCOMING EVENT"/>
+                        <div class="flex flex-col h-72 mt-12 pb-12">
+                            <div class="font-extrabold text-xl leading-180">{{ event.title }}</div>
+                            <div class="mt-8 text-base font-semibold leading-170">{{ event.timing }}</div>
+                            <div class="mt-16 text-base leading-normal">{{ event.description }}</div>
+                        </div>
+                        <div class="flex flex-col mt-12 pb-10" v-for="(eventUpdates, index) in eventUpdates" v-bind:key="eventUpdates">
+                            <div class="font-semibold text-base leading-170">{{ index+1 }} Updates</div>
+                            <UpdateCard>
+                                title= {{ eventUpdates.title }}
+                                description= {{ eventUpdate.description }}
+                            </UpdateCard>
+                        </div>
+                        <div class="mt-10 font-extrabold text-base text-red mb-8 leading-170">Know more</div>
                     </div>
-                    <div class="flex flex-col mt-12 pb-10">
-                        <div class="font-semibold text-base leading-170">2 Updates</div>
-                        <UpdateCard
-                        title="Answering Your Questions"
-                        description="As many of you have similar questions regarding teams and topics, we decided to come up with this post."/>
-                        <UpdateCard
-                        title="Opening Talk"
-                        description="28th September, 2018 | 7:00 PM | LHC 005"/>
-                    </div>
-                    <div class="mt-10 font-extrabold text-base text-red mb-8 leading-170">Know more</div>
                 </div>
-                <div class="mt-10 w-feed h-auto shadow-feed pl-10 pt-10 pr-navbar mb-64">
-                    <Label text="PAST EVENT"/>
-                    <div class="flex flex-col pb-14">
-                        <div class="mt-14 font-extrabold text-xl leading-180">LINUX PARTY</div>
-                        <div class="mt-8 font-semibold text-base leading-170">19th August, 2018</div>
-                        <div class="mt-14 text-base leading-normal">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. </div>
+                <div v-if="event.types == 'past'">
+                    <div class="mt-10 w-feed h-auto shadow-feed pl-10 pt-10 pr-navbar mb-64">
+                        <Label text="PAST EVENT"/>
+                        <div class="flex flex-col pb-14">
+                            <div class="mt-14 font-extrabold text-xl leading-180">{{ event.title }}</div>
+                            <div class="mt-8 font-semibold text-base leading-170">{{ event.timing }}</div>
+                            <div class="mt-14 text-base leading-normal">{{ event.description }}</div>
+                        </div>
+                        <div class="mt-10 font-extrabold text-base text-green mb-8 leading-170">Know more</div>
                     </div>
-                    <div class="mt-10 font-extrabold text-base text-green mb-8 leading-170">Know more</div>
                 </div>
             </div>
             <div class="flex flex-col ml-10">
                 <div class="flex flex-row">
-                    <div class="w-event h-auto shadow-feed pl-10 pt-10 pr-10">
-                        <Label text="APP UPDATE"/>
-                        <div class="flex flex-col mt-10 pb-10">
-                            <div class="font-extrabold text-xl leading-180">MUZI</div>
-                            <div class="mt-20 text-base leading-normal">The album Haider by Vishal Bharadwaj is now available on Muzi.</div>
+                    <div v-if="event.types == 'app'">
+                        <div class="w-event h-auto shadow-feed pl-10 pt-10 pr-10">
+                            <Label text="APP UPDATE"/>
+                            <div class="flex flex-col mt-10 pb-10">
+                                <div class="font-extrabold text-xl leading-180">{{ event.title }}</div>
+                                <div class="mt-20 text-base leading-normal">{{ event.description }}</div>
+                            </div>
+                            <div class="mt-8 font-extrabold text-base text-blue mb-8 leading-170">Check Now</div>
                         </div>
-                        <div class="mt-8 font-extrabold text-base text-blue mb-8 leading-170">Check Now</div>
-                    </div>
-                    <div class="w-event h-auto shadow-feed pl-10 pt-10 pr-10 ml-10">
-                        <Label text="ONLINE COMPETITION"/>
-                        <div class="flex flex-col mt-10 pb-10">
-                            <div class="font-extrabold text-xl leading-180">CODEBLITZ</div>
-                            <div class="mt-6 font-semibold text-base leading-170">25th September, 2018</div>
-                            <div class="mt-14 text-base leading-normal">Competetion starts at 9:00 PM on CodeVillage.</div>
+                        <div class="w-event h-auto shadow-feed pl-10 pt-10 pr-10 ml-10">
+                            <Label text="ONLINE COMPETITION"/>
+                            <div class="flex flex-col mt-10 pb-10">
+                                <div class="font-extrabold text-xl leading-180">{{ event.title }}</div>
+                                <div class="mt-6 font-semibold text-base leading-170">{{ event.timing }}</div>
+                                <div class="mt-14 text-base leading-normal">{{ event.description }}</div>
+                            </div>
+                            <div class="mt-8 font-extrabold text-base text-yellow mb-8 leading-170">Check Now</div>
                         </div>
-                        <div class="mt-8 font-extrabold text-base text-yellow mb-8 leading-170">Check Now</div>
                     </div>
                 </div>
-                <div class="mt-10 w-feed h-auto shadow-feed pl-10 pt-10 pr-navbar mb-64">
-                    <Label text="PAST EVENT"/>
-                    <div class="flex flex-col pb-14">
-                        <div class="mt-14 font-extrabold text-xl leading-180">WORKSHOP ON WEB DEVELOPMENT</div>
-                        <div class="mt-8 font-semibold text-base leading-170">8th September, 2018</div>
-                        <div class="mt-14 text-base leading-normal">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</div>
+                <div v-if="event.types == 'past'">
+                    <div class="mt-10 w-feed h-auto shadow-feed pl-10 pt-10 pr-navbar mb-64">
+                        <Label text="PAST EVENT"/>
+                        <div class="flex flex-col pb-14">
+                            <div class="mt-14 font-extrabold text-xl leading-180">{{ event.title }}</div>
+                            <div class="mt-8 font-semibold text-base leading-170">{{ event.timing}}</div>
+                            <div class="mt-14 text-base leading-normal">{{ event.description }}</div>
+                        </div>
+                        <div class="flex flex-col pb-12 mt-14">
+                            <div class="font-semibold text-base leading-normal">1 Update</div>
+                            <div class="mt-8 font-extrabold text-base leading-180">POST WORKSHOP PHOTOS</div>
+                        </div>
+                        <div class="mt-10 font-extrabold text-base text-purple mb-8 leading-180">Know more</div>
                     </div>
-                    <div class="flex flex-col pb-12 mt-14">
-                        <div class="font-semibold text-base leading-normal">1 Update</div>
-                        <div class="mt-8 font-extrabold text-base leading-180">POST WORKSHOP PHOTOS</div>
-                    </div>
-                    <div class="mt-10 font-extrabold text-base text-purple mb-8 leading-180">Know more</div>
                 </div>
             </div>
         </div>
@@ -89,6 +95,26 @@ export default {
             onlineCompetition: {},
             eventUpdates: {}
         }
-    }
+    },
+    mounted() {
+        axios
+            .get('http://0.0.0.0:8000/api/news/?format=json')
+            .then(response => {
+                let event = {}
+                for(let i = 0; i < response.data.length; i++) {
+                    event[response.data[i]["title"]] = response.data[i]
+                }
+                this.event = Object.assign({}, this.event, event);
+            })
+        axios
+            .get('http://0.0.0.0:8000/api/news/updates/?format=json')
+            .then(response => {
+                let eventUpdates = {}
+                for(let i = 0; i < response.data.length; i++) {
+                    eventUpdates[response.data[i]["title"]] = response.data[i]
+                }
+                this.eventUpdates = Object.assign({}, this.eventUpdates, eventUpdates);
+            })
+    },
 };
 </script>

1734dc39c5807014f60eab5fd6c82ff44ea47f07
AyanChoudhary <darkrider251099@gmail.com>
2019-12-24 20:05:17 +0530

    fix:image path in sister_group component

--- frontend/src/components/SisterGroups.vue
+++ frontend/src/components/SisterGroups.vue
@@ -1,25 +1,52 @@
 <template>
-  <div class="sister-groups w-auto flex flex-col md:content-center ml-8 mr-8 md:ml-0 md:mr-0 pt-60">
-    <div class="font-black  md:self-center text-3xl leading-loose md:w-feed md:text-center">Sister Groups</div>
-    <div class="font-base md:font-semibold md:self-center text-base md:text-center leading-normal md:w-sister mt-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac lacinia nisi, ac maximus sem. Ut eu malesuada nisl. Pellentesque sagittis fringilla tempor. Pellentesque sit amet augue sit amet quam feugiat porta vitae sit amet ligula.</div>
-    <div class="sister flex flex-row self-center mt-32 justify-between md:w-groups">
-      <div>
-          <Sister v-bind:url="dsg" title="DATA SCIENCE GROUP" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac lacinia nisi, ac maximus sem."/>
-      </div>
-      <div>
-          <Sister v-bind:url="pag" title="PROGRAMMING & ALGORITHMS GROUP" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac lacinia nisi, ac maximus sem."/>
-      </div>
-      <div>
-          <Sister v-bind:url="infosec" title="INFOSEC IITR" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac lacinia nisi, ac maximus sem."/>
-      </div>
+    <div
+        class="sister-groups w-auto flex flex-col md:content-center ml-8 mr-8 md:ml-0 md:mr-0 pt-60"
+    >
+        <div
+            class="font-black  md:self-center text-3xl leading-loose md:w-feed md:text-center"
+        >
+            Sister Groups
+        </div>
+        <div
+            class="font-base md:font-semibold md:self-center text-base md:text-center leading-normal md:w-sister mt-12"
+        >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac
+            lacinia nisi, ac maximus sem. Ut eu malesuada nisl. Pellentesque
+            sagittis fringilla tempor. Pellentesque sit amet augue sit amet quam
+            feugiat porta vitae sit amet ligula.
+        </div>
+        <div
+            class="sister flex flex-row self-center mt-32 justify-between md:w-groups"
+        >
+            <div>
+                <Sister
+                    v-bind:url="dsg"
+                    title="DATA SCIENCE GROUP"
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac lacinia nisi, ac maximus sem."
+                />
+            </div>
+            <div>
+                <Sister
+                    v-bind:url="pag"
+                    title="PROGRAMMING & ALGORITHMS GROUP"
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac lacinia nisi, ac maximus sem."
+                />
+            </div>
+            <div>
+                <Sister
+                    v-bind:url="infosec"
+                    title="INFOSEC IITR"
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac lacinia nisi, ac maximus sem."
+                />
+            </div>
+        </div>
+        <div class="mt-12 md:hidden"><SisterGallery /></div>
     </div>
-    <div class="mt-12 md:hidden"><SisterGallery/></div>
-  </div>
 </template>
 
 <script>
 import Sister from "@/components/about/Sister.vue";
-import SisterGallery  from "@/components/about/SisterGallery.vue";
+import SisterGallery from "@/components/about/SisterGallery.vue";
 import dsg from "@/assets/images/dsg.svg";
 import pag from "@/assets/images/pag.svg";
 import infosec from "@/assets/images/infosec.svg";
@@ -28,13 +55,20 @@ export default {
     components: {
         Sister,
         SisterGallery
+    },
+    data: function initData() {
+        return {
+            dsg: dsg,
+            pag: pag,
+            infosec: infosec
+        };
     }
-}
+};
 </script>
 
 <style lang="scss" scoped>
 @media screen and (max-width: 576px) {
-    .sister{
+    .sister {
         display: none;
     }
 }
--- frontend/src/components/about/SisterGallery.vue
+++ frontend/src/components/about/SisterGallery.vue
@@ -2,15 +2,42 @@
     <div class="w-full">
         <div class="flex flex-col w-auto h-96 shadow-sister">
             <div class="flex flex-row mt-8 self-center">
-                <div><img class="ml-8 w-56 h-40" v-bind:src="slides[queue-1].image"/></div>
-                <div class="mt-12 ml-12 leading-160 font-semibold text-lg w-96">{{slides[queue-1].title}}</div>
+                <div>
+                    <img
+                        class="ml-8 w-56 h-40"
+                        v-bind:src="slides[queue - 1].image"
+                    />
+                </div>
+                <div class="mt-12 ml-12 leading-160 font-semibold text-lg w-96">
+                    {{ slides[queue - 1].title }}
+                </div>
             </div>
-            <div class="mt-6 ml-10 mr-12 text-base leading-normal">{{slides[queue-1].description}}</div>
+            <div class="mt-6 ml-10 mr-12 text-base leading-normal">
+                {{ slides[queue - 1].description }}
+            </div>
         </div>
         <div class="text-center">
-            <span class="h-4 w-4 rounded-50 inline-block mt-16" v-bind:class="{'bg-black': queue===1, 'bg-carousel': queue!==1}"/>
-            <span class="h-4 w-4 rounded-50 inline-block mt-16 ml-4" v-bind:class="{'bg-black': queue===2, 'bg-carousel': queue!==2}"/>
-            <span class="h-4 w-4 rounded-50 inline-block mt-16 ml-4" v-bind:class="{'bg-black': queue===3, 'bg-carousel': queue!==3}"/>
+            <span
+                class="h-4 w-4 rounded-50 inline-block mt-16"
+                v-bind:class="{
+                    'bg-black': queue === 1,
+                    'bg-carousel': queue !== 1
+                }"
+            />
+            <span
+                class="h-4 w-4 rounded-50 inline-block mt-16 ml-4"
+                v-bind:class="{
+                    'bg-black': queue === 2,
+                    'bg-carousel': queue !== 2
+                }"
+            />
+            <span
+                class="h-4 w-4 rounded-50 inline-block mt-16 ml-4"
+                v-bind:class="{
+                    'bg-black': queue === 3,
+                    'bg-carousel': queue !== 3
+                }"
+            />
         </div>
     </div>
 </template>
@@ -27,35 +54,36 @@ export default {
                 {
                     image: dsg,
                     title: "DATA SCIENCE GROUP",
-                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac lacinia nisi, ac maximus sem."
+                    description:
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac lacinia nisi, ac maximus sem."
                 },
                 {
                     image: pag,
                     title: "PROGRAMMING & ALGORITHMS GROUP",
-                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac lacinia nisi, ac maximus sem."
+                    description:
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac lacinia nisi, ac maximus sem."
                 },
                 {
                     image: infosec,
                     title: "INFOSEC IITR",
-                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac lacinia nisi, ac maximus sem."
+                    description:
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac lacinia nisi, ac maximus sem."
                 }
             ],
             queue: 1
-        }
+        };
     },
     methods: {
         carousel: function() {
             setInterval(() => {
-                if (this.queue%3 !== 0) {
-                    this.queue++
-                }
-                else
-                    this.queue = 1
-                }, 2000)
+                if (this.queue % 3 !== 0) {
+                    this.queue++;
+                } else this.queue = 1;
+            }, 2000);
         }
     },
     mounted() {
-        this.carousel()
+        this.carousel();
     }
 };
 </script>

33df7ee6866b6312befd04e3a4e0367b195bbd64
shubhamgupta2956 <shubham.gupta2956@gmail.com>
2019-12-25 20:51:15 +0530

    feat: event page events rendering

--- frontend/src/components/Update.vue
+++ frontend/src/components/Update.vue
@@ -17,7 +17,7 @@
         <div class="md:w-image">
             <img
                 class="mt-12 md:mt-0 md:ml-44 md:w-image shadow-image"
-                src="https://s3-alpha-sig.figma.com/img/4e0c/35b7/ecb625bceac942a2a05fc3ef05c30395?Expires=1578268800&Signature=LggaMRc8MXKlEU8Xo6-iwLVZrzX0aiz9DLCfvO21m3TEQHh39Y4XD6QLJlKYeBv8~WzEvmb4OqaP~WIvWxpKap~6kspvehR2a5ufqtaba8giP~Lkh9Y6tmfxx6lbkOtEyQwVb2kbKM-D6Doh0Nz6cIMBaGBd3yM2vWMrZ8mAYqxqPkt-u~NM9YCRdpIg9EAEIMNrTqVuETeNL4h-poR0MtskLxmg7bGch2ldQCyZS4Gz406NvqFI~HsRnvnsbfO32eH~lgygj4UqlyDBNWvUpe6NgTFVdbzmXDT4uc9~uwln1xwwQcLViJ5bOv3G73Sxhp1ndVxe6D5DyAvEGaHCOw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+                :src="image"
             />
         </div>
     </div>
@@ -33,7 +33,8 @@ export default {
         description1: String,
         description2: String,
         description3: String,
-        url: String
+        url: String,
+        image: String
     }
 };
 </script>

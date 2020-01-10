4d4bb1b33e8b1e513d6b184282f038637820c582
AyanChoudhary <darkrider251099@gmail.com>
2020-01-10 13:24:53 +0530

    fix: improve image load time in gallery and increased display time

--- frontend/src/components/Gallery.vue
+++ frontend/src/components/Gallery.vue
@@ -2,33 +2,59 @@
     <div class="gallery flex flex-col w-auto sm:w-gallery">
         <img
             class="sm:h-128 sm:w-180 rounded-lg"
-            v-bind:src="slides[queue-1].image"
+            v-bind:src="slides[queue - 1].image"
             alt="image"
         />
         <div
-            class="textbox w-auto ml-6 mr-6 h-auto min-h-64 -mt-32 pr-6 sm:w-120 sm:h-auto sm:min-h-88 sm:ml-98 sm:-mt-gallery bg-white z-20"
+            class="textbox w-auto ml-6 mr-6 h-auto min-h-88 -mt-32 pr-6 sm:w-120 sm:h-auto sm:min-h-88 sm:ml-98 sm:-mt-gallery bg-white z-20"
         >
-            <div class="font-extrabold text-2xl sm:text-lg mt-10 ml-10">{{slides[queue-1].title}}</div>
+            <div class="font-extrabold text-2xl sm:text-lg mt-10 ml-10">
+                {{ slides[queue - 1].title }}
+            </div>
             <div
                 class="sm:w-104 leading-170 text-grey text-xl sm:text-base mt-8 ml-10 mb-8"
             >
-                {{slides[queue-1].description}}
+                {{ slides[queue - 1].description }}
             </div>
         </div>
         <div class="self-center sm:self-start sm:-mt-32">
-            <span class="h-4 w-4 sm:h-3 sm:w-3 rounded-50 inline-block mt-16" v-bind:class="{'bg-black': queue===1, 'bg-carousel': queue!==1}"/>
-            <span class="h-4 w-4 sm:h-3 sm:w-3 rounded-50 inline-block mt-16 ml-4" v-bind:class="{'bg-black': queue===2, 'bg-carousel': queue!==2}"/>
-            <span class="h-4 w-4 sm:h-3 sm:w-3 rounded-50 inline-block mt-16 ml-4" v-bind:class="{'bg-black': queue===3, 'bg-carousel': queue!==3}"/>
-            <span class="h-4 w-4 sm:h-3 sm:w-3 rounded-50 inline-block mt-16 ml-4" v-bind:class="{'bg-black': queue===4, 'bg-carousel': queue!==4}"/>
+            <span
+                class="h-4 w-4 sm:h-3 sm:w-3 rounded-50 inline-block mt-16"
+                v-bind:class="{
+                    'bg-black': queue === 1,
+                    'bg-carousel': queue !== 1
+                }"
+            />
+            <span
+                class="h-4 w-4 sm:h-3 sm:w-3 rounded-50 inline-block mt-16 ml-4"
+                v-bind:class="{
+                    'bg-black': queue === 2,
+                    'bg-carousel': queue !== 2
+                }"
+            />
+            <span
+                class="h-4 w-4 sm:h-3 sm:w-3 rounded-50 inline-block mt-16 ml-4"
+                v-bind:class="{
+                    'bg-black': queue === 3,
+                    'bg-carousel': queue !== 3
+                }"
+            />
+            <span
+                class="h-4 w-4 sm:h-3 sm:w-3 rounded-50 inline-block mt-16 ml-4"
+                v-bind:class="{
+                    'bg-black': queue === 4,
+                    'bg-carousel': queue !== 4
+                }"
+            />
         </div>
     </div>
 </template>
 
 <script>
-import lectures from "@/assets/images/lectures.svg";
-import hackathons from "@/assets/images/hackathons.svg";
-import workshops from "@/assets/images/workshops.svg";
-import competitions from "@/assets/images/competitions.svg";
+import lectures from "@/assets/images/lectures.png";
+import hackathons from "@/assets/images/hackathons.png";
+import workshops from "@/assets/images/workshops.png";
+import competitions from "@/assets/images/competitions.png";
 export default {
     name: "Gallery",
     data: function initData() {
@@ -68,7 +94,7 @@ export default {
                 if (this.queue % 4 !== 0) {
                     this.queue++;
                 } else this.queue = 1;
-            }, 2000);
+            }, 4000);
         }
     },
     mounted() {
